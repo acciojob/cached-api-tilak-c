4017efc59f68a2d216ac8775bc9dc636386a4fdc
Tilak <adireddytilak@gmail.com>
2025-10-31 11:50:05 +0530

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,39 @@
 
-import React from "react";
+import React,{useMemo,useEffect,useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [posts,setPosts]=useState([]);
+  const url="https://jsonplaceholder.typicode.com/posts";
+  useEffect(()=>{
+    fetch(url)
+    .then((response)=>response.json())
+    .then((data)=>setPosts(data))
+    .catch((error)=>console.error("Error fetching users:",error))
+  }
+    ,[]);
+  const cachedPosts=useMemo(()=>{
+    console.log("Memoizing Posts");
+    return posts;
+  },[posts]);
+
   return (
     <div>
+      <ul>
         {/* Do not remove the main div */}
+        {cachedPosts.length === 0 ? (
+          <p>Loading...</p>
+        ) : (
+          cachedPosts.map((post, index) => (
+            <li key={index}>
+              <h3>{post.title}</h3>
+              <p>{post.body}</p>
+            </li>
+          ))
+        )}
+      </ul>
     </div>
-  )
+  );
 }
 
 export default App
